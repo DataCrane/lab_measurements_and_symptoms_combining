3fe13f768b7dda0e5814697d586200fc8893ea2b
Michał <zuraw3@gmail.com>
2018-10-05 23:39:28 +0200

    NO COMMENT

--- combine_tables.py
+++ combine_tables.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 # from openpyxl.workbook import Workbook
 
 stratigraphy = pd.read_csv('stratigraphy.csv')
@@ -10,5 +12,25 @@ lab_data = pd.read_csv('lab_data.csv')
 symptoms = pd.read_csv('symptoms.csv')
 # print(symptoms.head())
 
+# print(stratigraphy.info())
+# print(lab_data.info())
 
+# df1 = pd.merge(stratigraphy, lab_data, how='inner', on='MYUWI', validate='m:m')
+
+a = lab_data.DEPTH.values
+bh = stratigraphy.BOTTOM.values
+bl = stratigraphy.TOP.values
+
+i, j = np.where((a[:, None] >= bl) & (a[:, None] <= bh))
+
+df1 = pd.DataFrame(
+    np.column_stack([lab_data.values[i], stratigraphy.values[j]]),
+    columns=lab_data.columns.append(stratigraphy.columns)
+    )
+
+
+
+writer = pd.ExcelWriter('SHIP 1.xlsx')
+df1.to_excel(writer,'SHIP 1')
+writer.save()
 
