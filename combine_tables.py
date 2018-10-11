0c16d233a7dc70aa881fbb24388434db69033a69
Michał <zuraw3@gmail.com>
2018-10-11 14:49:51 +0200

    Properly merged and filtered by range lab_data and stratigraphy, one table(symptoms) left to merge.

--- combine_tables.py
+++ combine_tables.py
@@ -1,36 +1,22 @@
 import pandas as pd
-import numpy as np
 
-# from openpyxl.workbook import Workbook
+# read datasets
+stratigraphy = pd.read_csv('stratigraphy.csv', na_values=-999.25)
+lab_data = pd.read_csv('lab_data.csv', na_values=-999.25).drop(['TOP', 'BOTTOM'], axis=1)
 
-stratigraphy = pd.read_csv('stratigraphy.csv')
-# print(stratigraphy.head())
-
-lab_data = pd.read_csv('lab_data.csv')
-# print(lab_data.head())
-
-symptoms = pd.read_csv('symptoms.csv')
+# symptoms = pd.read_csv('symptoms.csv')
 # print(symptoms.head())
 
-# print(stratigraphy.info())
-# print(lab_data.info())
+# merge dataframes do df
+df = lab_data.merge(stratigraphy, how='left', on=['MYUWI'])
 
-# df1 = pd.merge(stratigraphy, lab_data, how='inner', on='MYUWI', validate='m:m')
+# filter by range
+lab_list=list(lab_data.columns.values) + ['STRATIGRAPHY_SYM', 'LITHOLOGY']
+df = df[(df.DEPTH >= df.TOP) & (df.DEPTH <= df.BOTTOM)][lab_list].reset_index(drop=True)
 
-a = lab_data.DEPTH.values
-bh = stratigraphy.BOTTOM.values
-bl = stratigraphy.TOP.values
-
-i, j = np.where((a[:, None] >= bl) & (a[:, None] <= bh))
-
-df1 = pd.DataFrame(
-    np.column_stack([lab_data.values[i], stratigraphy.values[j]]),
-    columns=lab_data.columns.append(stratigraphy.columns)
-    )
-
-
-
-writer = pd.ExcelWriter('SHIP 1.xlsx')
-df1.to_excel(writer,'SHIP 1')
+# write to xlsx
+writer = pd.ExcelWriter('merged lab_str.xlsx')
+df.to_excel(writer,'merged lab_str')
 writer.save()
+
 
