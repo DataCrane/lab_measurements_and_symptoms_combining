9c8b4c124ae90d6d3b5106f0b00cb383a004ae7f
Michał <zuraw3@gmail.com>
2018-12-10 19:06:25 +0100

    def labels

--- combine_tables.py
+++ combine_tables.py
@@ -46,7 +46,7 @@ Rdf = Rdf[(Rdf.DEPTH >= Rdf.TOP) & (Rdf.DEPTH <= Rdf.BOTTOM)][columns_list3].res
 # df2 = df2[((df2.START >= df2.TOP)
 #            & (df2.STOP >= df2.TOP)
 #            & (df2.START <= df2.BOTTOM)
-#            & (df2.STOP <=df2.BOTTOM))
+#            & (df2.STOP <= df2.BOTTOM))
 #           | ((df2.START <= df2.BOTTOM) & (df2.STOP >= df2.TOP))][columns_list2].reset_index(drop=True)
 
 
@@ -68,24 +68,26 @@ barchart.set_title("samples frequency", fontsize=18)
 barchart.set_ylabel("count", fontsize=18)
 # barchart.set_yticks(y_t)
 
+def labels(chart=None):
+    # create a list to collect the plot.patches data
+    totals = []
 
-# create a list to collect the plot.patches data
-totals = []
+    # find the values and append to list
+    for i in chart.patches:
+        totals.append(i.get_height())
 
-# find the values and append to list
-for i in barchart.patches:
-    totals.append(i.get_height())
+    # set individual bar labels using above list
+    total = sum(totals)
 
-# set individual bar labels using above list
-total = sum(totals)
+    # set individual bar labels using above list
+    for i in chart.patches:
+        # get_x pulls left or right; get_height pushes up or down
+        chart.text(i.get_x(), i.get_height(), \
+                # str(round((i.get_height()/total)*100, 2))+'%', fontsize=15, # PERCENT
+                str(round(i.get_height(), 2)),  va='center', fontsize=6, color='black')
+    plot.show()
 
-# set individual bar labels using above list
-for i in barchart.patches:
-    # get_x pulls left or right; get_height pushes up or down
-    barchart.text(i.get_x(), i.get_height(), \
-            # str(round((i.get_height()/total)*100, 2))+'%', fontsize=15, # PERCENT
-            str(round(i.get_height(), 2)),  va='center', fontsize=6, color='black')
-plot.show()
+labels(chart=barchart)
 
 
 
@@ -96,70 +98,31 @@ median_PERM_BIT = (Rdf
                    )
 
 ### prepare plot
-
 barchart2 = median_PERM_BIT.plot.bar()
-
-
 barchart2.set_title("median PERMEABILITY and BITUMINS", fontsize=18)
 barchart2.set_yscale("log", nonposy='clip')
 # y_t = np.arange(0, 900, 100).tolist() # optional
 barchart2.set_ylabel("", fontsize=18)
 # barchart.set_yticks(y_t)
 
-
-# create a list to collect the plot.patches data
-totals = []
-
-# find the values and append to list
-for i in barchart2.patches:
-    totals.append(i.get_height())
-
-# set individual bar labels using above list
-total = sum(totals)
-
-# set individual bar labels using above list
-for i in barchart2.patches:
-    # get_x pulls left or right; get_height pushes up or down
-    barchart2.text(i.get_x(), i.get_height(), \
-            # str(round((i.get_height()/total)*100, 2))+'%', fontsize=15, # PERCENT
-            str(round(i.get_height(), 2)),  va='center', fontsize=6, color='black')
-plot.show()
+# create labels
+labels(barchart2)
 
 
 
 ### What is the median POROSITY per MYUWI(well id) and STRATIGRAPHY_SYM ? ? ?
-
 median_POR = (Rdf
               .groupby(['MYUWI', 'STRATIGRAPHY_SYM'])
               ['POROSITY'].median()
               )
 
 ### prepare plot
-
 barchart3 = median_POR.plot.bar()
-
-
 barchart3.set_title("median POROSITY", fontsize=18)
 # barchart2.set_yscale("log", nonposy='clip')
 # y_t = np.arange(0, 900, 100).tolist() # optional
 barchart3.set_ylabel("", fontsize=18)
 # barchart.set_yticks(y_t)
 
-
-# create a list to collect the plot.patches data
-totals = []
-
-# find the values and append to list
-for i in barchart3.patches:
-    totals.append(i.get_height())
-
-# set individual bar labels using above list
-total = sum(totals)
-
-# set individual bar labels using above list
-for i in barchart3.patches:
-    # get_x pulls left or right; get_height pushes up or down
-    barchart3.text(i.get_x(), i.get_height(), \
-            # str(round((i.get_height()/total)*100, 2))+'%', fontsize=15, # PERCENT
-            str(round(i.get_height(), 2)),  va='center', fontsize=6, color='black')
-plot.show()
+# create labels
+labels(barchart3)
